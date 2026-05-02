d0cee55fa5b667913296849960de66b2b1297469
ZabihollahNamazi <zabihnamazi@gmail.com>
2026-05-02 13:15:44 +0100

    url update

--- frontend/quote-fetch.js
+++ frontend/quote-fetch.js
@@ -2,7 +2,7 @@ const quoteEl = document.getElementById("quote");
 const btn = document.getElementById("btn");
 
 async function getQuote() {
-const response = await fetch("https://zabihollahnamazi-decomposition-quotes-backend.hosting.codeyourfuture.io/");
+const response = await fetch("https://zabihollahnamazi-decomposition-quotes-frontend.hosting.codeyourfuture.io/");
 const text = await response.text();
 quoteEl.innerText = text;
 }
